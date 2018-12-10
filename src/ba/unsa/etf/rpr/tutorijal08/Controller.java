816caf1806ccab3f925f8b5bf28405c9718cafa5
shrbenic1 <shrbenic1@etf.unsa.ba>
2018-12-10 22:54:06 +0100

    Omogućena pretraga direktorija (trenutno bez korištenja threadinga)

--- src/ba/unsa/etf/rpr/tutorijal08/Controller.java
+++ src/ba/unsa/etf/rpr/tutorijal08/Controller.java
@@ -1,14 +1,38 @@
 package ba.unsa.etf.rpr.tutorijal08;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 
+import java.io.File;
 
 public class Controller {
     public TextField trazenaRijec;
+    public ListView<String> lista;
+    private ObservableList<String> observableList = FXCollections.observableArrayList();
 
-    public void trazi(ActionEvent actionEvent) {
-        System.out.println(trazenaRijec.getText());
+    public void walk (String pocetak, String rijec){
+        File root = new File(pocetak);
+        File[] list = root.listFiles();
+        if (list == null) {
+            return;
+        }
+        for (File f : list) {
+            if (f.isDirectory()) {
+                walk(f.getAbsolutePath(), rijec);
+            } else {
+                if(f.getAbsoluteFile().toString().contains(rijec)) {
+                    observableList.add(f.getAbsoluteFile().toString());
+                    lista.setItems(observableList);
+                    System.out.println("File:" + f.getAbsoluteFile());
+                }
+            }
+        }
     }
 
+    public void trazi(ActionEvent actionEvent) {
+        walk("C:\\Users", trazenaRijec.getText());
+    }
 }
