5e247981183c4f46c5f7bd80e652502425db911d
shrbenic1 <shrbenic1@etf.unsa.ba>
2018-12-10 20:03:39 +0100

    Napravljen property binding za traženu riječ

--- src/ba/unsa/etf/rpr/tutorijal08/Controller.java
+++ src/ba/unsa/etf/rpr/tutorijal08/Controller.java
@@ -1,4 +1,19 @@
 package ba.unsa.etf.rpr.tutorijal08;
 
+import javafx.beans.property.SimpleStringProperty;
+
 public class Controller {
+    private SimpleStringProperty trazenaRijec;
+
+    public Controller() {
+        trazenaRijec = new SimpleStringProperty("");
+    }
+
+    public SimpleStringProperty trazenaRijecProperty() {
+        return trazenaRijec;
+    }
+
+    public String getTrazenaRijec() {
+        return trazenaRijec.get();
+    }
 }
