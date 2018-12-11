4093795697dc9e0b9f2d322b50492580a1e67a00
shrbenic1 <shrbenic1@etf.unsa.ba>
2018-12-11 21:42:23 +0100

    Omogućena validacija poštanskog broja pri promjeni fokusa

--- src/ba/unsa/etf/rpr/tutorijal08/SlanjeController.java
+++ src/ba/unsa/etf/rpr/tutorijal08/SlanjeController.java
@@ -1,4 +1,46 @@
 package ba.unsa.etf.rpr.tutorijal08;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXML;
+import javafx.scene.control.Control;
+import javafx.scene.control.TextField;
+import org.controlsfx.validation.decoration.GraphicValidationDecoration;
+import org.controlsfx.validation.Severity;
+import org.controlsfx.validation.ValidationMessage;
+
 public class SlanjeController {
+    public TextField postanskiBroj;
+
+    private boolean validanPostanskiBroj(String n) {
+        return true;
+    }
+
+    @FXML
+    public void initialize() {
+        postanskiBroj.focusedProperty().addListener(new ChangeListener<Boolean>() {
+            @Override
+            public void changed(ObservableValue<? extends Boolean> obs, Boolean o, Boolean n) {
+                GraphicValidationDecoration graphicValidationDecoration = new GraphicValidationDecoration();
+                if (!n && !validanPostanskiBroj(postanskiBroj.getCharacters().toString())) {
+                    graphicValidationDecoration.applyValidationDecoration(new ValidationMessage() {
+                        @Override
+                        public String getText() {
+                            return "Neispravan poštanski broj!";
+                        }
+                        @Override
+                        public Severity getSeverity() {
+                        return Severity.ERROR;
+                    }
+                        @Override
+                        public Control getTarget() {
+                        return postanskiBroj;
+                    }
+                });
+            } else {
+                graphicValidationDecoration.removeDecorations(postanskiBroj);
+            }
+        }
+    });
+    }
 }
