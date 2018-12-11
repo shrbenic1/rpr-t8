82886670a3326859a9a8402d0f6588cce15ed885
shrbenic1 <shrbenic1@etf.unsa.ba>
2018-12-11 23:43:02 +0100

    Ukinuti pomoćni ispisi i biblioteke

--- src/ba/unsa/etf/rpr/tutorijal08/SlanjeController.java
+++ src/ba/unsa/etf/rpr/tutorijal08/SlanjeController.java
@@ -1,20 +1,12 @@
 package ba.unsa.etf.rpr.tutorijal08;
 
-import javafx.application.Platform;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
-import javafx.scene.control.Control;
 import javafx.scene.control.TextField;
-import org.controlsfx.validation.decoration.GraphicValidationDecoration;
-import org.controlsfx.validation.Severity;
-import org.controlsfx.validation.ValidationMessage;
 
 import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 
@@ -46,11 +38,9 @@ public class SlanjeController {
                     new Thread(() -> {
                         try {
                             if (validanPostanskiBroj(postanskiBroj.getCharacters().toString())) {
-                                System.out.println("OK");
                                 postanskiBroj.getStyleClass().removeAll("poljeNijeIspravno");
                                 postanskiBroj.getStyleClass().add("poljeIspravno");
                             } else {
-                                System.out.println("NOT OK");
                                 postanskiBroj.getStyleClass().removeAll("poljeIspravno");
                                 postanskiBroj.getStyleClass().add("poljeNijeIspravno");
                             }
